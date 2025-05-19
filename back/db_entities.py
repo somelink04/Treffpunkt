3d1f2d579964bf2037c8d3ff7d8777d26fbde6ea
DaveManTheFirst <gerhardinger7@gmail.com>
2025-05-19 16:45:29 +0200

    zusage is now bool added test data info tested classes

--- back/db_entities.py
+++ back/db_entities.py
@@ -1,8 +1,8 @@
 from datetime import date, datetime
 from typing import List, Optional
-from sqlalchemy import Integer, VARCHAR, CHAR, Float, ForeignKey, DATETIME
+from sqlalchemy import Integer, VARCHAR, CHAR, Float, ForeignKey, DATETIME, create_engine, BOOLEAN
 from sqlalchemy.dialects.mysql import TINYTEXT
-from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
@@ -49,21 +49,37 @@ class User(Base):
     USER_REGION: Mapped[int] = mapped_column(ForeignKey("Region.REGION_ID"))
 
 class UserTime(Base):
+    __tablename__ = "USER_TIME"
     USER_TIME_ID: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     HOUR_USER_TIME_ID: Mapped[int] = mapped_column(ForeignKey("Hour.HOUR_ID"))
     WEEKDAY_USER_TIME_ID: Mapped[int] = mapped_column(ForeignKey("Weekday.WEEKDAY_ID"))
     USER_USER_TIME_ID: Mapped[int] = mapped_column(ForeignKey("User.USER_ID"))
 
 class UserCategory(Base):
+    __tablename__ = "USER_CATEGORY"
     USER_USER_CATEGORY_ID: Mapped[int] = mapped_column(ForeignKey("User.USER_ID"), primary_key=True)
     CATEGORY_USER_CATEGORY_ID: Mapped[int] = mapped_column(ForeignKey("Category.CATEGORY_ID"), primary_key=True)
 
 class Event(Base):
+    __tablename__ = "EVENT"
     EVENT_ID: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     EVENT_TIME: Mapped[datetime] = mapped_column(DATETIME)
     EVENT_REGION: Mapped[int] = mapped_column(ForeignKey("Region.REGION_ID"))
     EVENT_CATEGORY: Mapped[int] = mapped_column(ForeignKey("Category.CATEGORY_ID"))
 
 class UserEvent(Base):
+    __tablename__ = "USER_EVENT"
     USER_USER_EVENT_ID: Mapped[int] = mapped_column(ForeignKey("User.USER_ID"), primary_key=True)
     EVENT_USER_EVENT_ID: Mapped[int] = mapped_column(ForeignKey("Event.EVENT_ID"), primary_key=True)
+    USER_EVENT_ZUSAGE: Mapped[bool] = mapped_column(BOOLEAN)
+
+## how to connect and access: see below
+#engine = create_engine("mysql+pymysql://treffpunkt:b55tQKAc0z2K0hluWSo7Zxq2cMs9pTgx@localhost/TREFFPUNKT_DB")
+
+#Session = sessionmaker(bind=engine)
+#session = Session()
+
+#result = session.query(Category).all()
+
+#for r in result:
+#    print(r.CATEGORY_NAME, r.CATEGORY_DESCRIPTION)
