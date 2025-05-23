8216a0af04508db071508f5f18786bee2308561e
Anja Weig <a.weig1@oth-aw.de>
2025-05-23 13:58:45 +0200

    More querying - no testing

--- back/db_api.py
+++ back/db_api.py
@@ -1,6 +1,8 @@
 from db_entities import *
 from sqlalchemy import text
 
+from db_entities import UserTime
+
 #Create Session
 engine = create_engine("mysql+pymysql://treffpunkt:b55tQKAc0z2K0hluWSo7Zxq2cMs9pTgx@localhost/TREFFPUNKT_DB")
 Session = sessionmaker(bind=engine)
@@ -56,7 +58,7 @@ def get_all_user_auth ():
 
     return user_auth
 
-#Alternativ: Einzelner User?
+#Alternativ: Suche nach User über Username
 def get_user_auth(user_name):
     """
     Gets one user auth data from the database.
@@ -70,16 +72,16 @@ def get_user_auth(user_name):
             print ("No User with this Username in DB.")
             return None
 
-
-
-#ohne passwort? Gleich hier raus oder handelt back?
+#--TODO: Brauchen wir hier noch was, was USER_GENDER, USER_REGION auflöst? Weil so bekommen wir vorraussichtlich 1, 39043 zurück
 def get_user (user_id):
     """
     :param user_id: ID of the searched User
-    :return: The searched user (User object)
+    :return: The user_name of the searched user (User object) without password.
     """
     with Session() as session:
-        user = session.query(User).filter_by(USER_ID=user_id).first()
+        user = session.query(User).with_entities(User.USER_ID, User.USER_FIRSTNAME, User.USER_SURNAME,
+                                                 User.USER_BIRTHDATE, User.USER_USERNAME, User.USER_EMAIL,
+                                                 User.USER_GENDER, User.USER_REGION).filter_by(USER_ID=user_id).first()
         if user:
             return user
         else:
@@ -88,38 +90,120 @@ def get_user (user_id):
 
 def get_all_users ():
     """
-    :return: list of all users
+    :return: list of all users without password.
     """
     with Session() as session:
-        user = session.query(User).all()
+        user = session.query(User).with_entities(User.USER_ID, User.USER_FIRSTNAME, User.USER_SURNAME,
+                                                 User.USER_BIRTHDATE, User.USER_USERNAME, User.USER_EMAIL,
+                                                 User.USER_GENDER, User.USER_REGION).all()
         return user
 
 
-#Get User_Settings
-#--TODO
-'''
-#hä? lieber einzeln holen? ist sonst schon recht convoluted ;D 
-def get_user_settings (user_id):
- #loopen
+#Write User Settings
+def add_user_time (user_id, user_hour, user_weekday):
+    """
+    Sets new UserTime in UserTime Table (One Hour on One Weekday).
+    ->Mapping Front (entering hour start-end for each weekday) to DB in Back
+    :param user_id: ID of the User
+    :param user_hours: ID of the hour
+    :param weekday: ID of the weekday
+    """
     with Session() as session:
-        user_region = session.query(User).with_entities(User.USER_REGION).filter_by(USER_ID=user_id).first()
-        user_hour = session.query(UserTime).with_entities (UserTime.HOUR_USER_TIME_ID).filter_by(USER_USER_TIME_ID=user_id).all()
-        print (user_region)
-        for entity in user_hour:
-            print (entity)
-        user_weekday_ids = session.query(UserTime).with_entities (UserTime.WEEKDAY_USER_TIME_ID).filter_by(USER_USER_TIME_ID=user_id).all()
-        user_category_ids = session.query(UserCategory).with_entities(UserCategory.CATEGORY_USER_CATEGORY_ID).filter_by(USER_USER_CATEGORY_ID=user_id).all()
-        for user_weekday_ids in user_weekday_ids:
-            user_weekday = session.query(Hour).filter_by(USER_HOUR_USER_TIME_ID=user_weekday_ids).all()
-        for user_category_ids in user_category_ids:
-            user_category_ = session.query(Weekday).filter_by(USER_WEEDAY_USER_TIME_ID=user_weekday_ids).all()
+        new_user_time = UserTime()
+        new_user_time.USER_USER_TIME_ID = user_id
+        new_user_time.HOUR_USER_TIME_ID = user_hour
+        new_user_time.WEEKDAY_USER_TIME_ID = user_weekday
+        session.add(new_user_time)
+        session.commit()
 
-'''
-#Write User_Settings - evtl auch einzeln
-#--TODO
+def add_user_category (user_id, category_id):
+    """
+    Sets new UserCategory in UserCategory Table
+    :param user_id: ID of the User
+    :param category_id: ID of the Category
+    """
+    with Session() as session:
+        new_user_category = UserCategory()
+        new_user_category.CATEGORY_USER_CATEGORY_ID = category_id
+        new_user_category.USER_USER_CATEGORY_ID = user_id
+        session.add(new_user_category)
+        session.commit()
 
-#Write New_Event
-#--TODO
+#Depends on the matching algorithm, has to be changed possibly
+def add_user_event(user_id, event_id):
+    """
+    Sets new UserEvent in UserEvent Table
+    :param user_id: ID of the User
+    :param event_id: ID of the Event
+    """
+    with Session() as session:
+        new_user_event = UserEvent()
+        new_user_event.EVENT_USER_EVENT_ID = event_id
+        new_user_event.USER_USER_EVENT_ID = user_id
+        session.add(new_user_event)
+        session.commit()
+
+#--TODO:Nicht sicher ob das so funktioniert->TEST!
+def add_zusage(zusageflag, user_event_id):
+    """
+    Sets new confirmationflag in UserEvent Table
+    :param zusageflag: Flag if user confirmed to Event or not
+    :param user_event_id: ID of the Event
+    """
+    with Session() as session:
+        user_event = session.query(UserEvent).filter_by(USER_EVENT_ID=user_event_id).first()
+        if user_event:
+            user_event.USER_EVENT_ZUSAGE = zusageflag
+            session.commit()
+        else:
+            print ("No such UserEvent in DB.")
+
 
 #Get Events
-#--TODO
+def get_confirmed_user_event(user_id):
+    """
+    Get only confirmed UserEvent from the database.
+    :param user_id: ID of the User
+    :return: Events the user confirmed to.
+    """
+    with Session() as session:
+        user_events = session.query(UserEvent).filter(USER_USER_EVENT_ID = user_id).all()
+        for i in range (len(user_events)):
+            if not user_events[i].USER_EVENT_ZUSAGE:
+                user_events.pop(i)
+        return user_events
+
+def get_unconfirmed_user_event(user_id):
+    with Session() as session:
+        user_events = session.query(UserEvent).filter(USER_USER_EVENT_ID = user_id).all()
+        for i in range (len(user_events)):
+            if user_events[i].USER_EVENT_ZUSAGE:
+                user_events.pop(i)
+        return user_events
+
+
+def get_all_user_events(user_id):
+    """
+    Get all from UserEvent table
+    :param user_id: the ID of the User
+    :return: All Events concerning this user
+    """
+    with Session() as session:
+        user_events = session.query(UserEvent).filter(USER_USER_EVENT_ID = user_id).all()
+        return user_events
+
+#Get User Settings
+def get_user_settings(user_id):
+    """
+    Get all settings for the matching
+    :param user_id: ID of the User
+    :return: Region of the User, Cathegories of the User, Times of the User (Weekday-Hour pair)
+    """
+    with Session() as session:
+        user_region = session.query(User).filter_by(USER_ID=user_id).first()
+        user_category = session.query(UserCategory).filter_by(USER_USER_CATEGORY_ID=user_id).first()
+        user_time = session.query(UserTime).filter_by(USER_USER_TIME_ID=user_id).first()
+        return user_region, user_category, user_time
+
+
+#TODO- Testen, evtl auch mal Testdaten schreiben für die unbefüllten Datenbank
