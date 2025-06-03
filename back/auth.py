9d97405cd17693a3ad28845358cc962b9ecb7edc
Paul Fröhlich <p.froehlich@oth-aw.de>
2025-06-03 12:21:43 +0200

    integrated db api functions into the login procedure

--- back/auth.py
+++ back/auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify, abort
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from db_api import get_user_auth, get_all_users
+import json
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -9,20 +11,34 @@ def init_app(app):
     jwt = JWTManager()
     jwt.init_app(app)
 
+def pack_at_data(username):
+    users = get_all_users()
+    for user in users:
+        if user.USER_USERNAME == username:
+            at_data = dict(
+                id=user.USER_ID,
+                username=user.USER_USERNAME
+            )
+            return create_access_token(identity=json.dumps(at_data))
+    return None
+
 @bp.route("/login", methods=['POST'])
 def login():
     username = request.json['username']
     password = request.json['password']
 
-    if username == 'test' and password == 'test':
-        access_token = create_access_token(identity=username)
-        return jsonify(dict(access_token=access_token))
-
+    db_password = get_user_auth(username)
+    if password == db_password:
+        access_token = pack_at_data(username)
+        return jsonify(access_token=access_token)
     else:
+        print("Password hashes dont match!")
+        print("Expected:", db_password, "- Got:", password)
         return abort(401)
 
 @bp.route("/ident", methods=['GET'])
 @jwt_required()
 def protected():
-    user = get_jwt_identity()
-    return jsonify(identity=user)
+    ident = get_jwt_identity()
+    user  = json.loads(ident)
+    return jsonify(identity=user['username'])
